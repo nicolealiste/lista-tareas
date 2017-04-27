13052f3176a6791959e81fa9e3d5581f7e332129
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-04-27 13:58:04 -0300

    validacion de datos y correccion de codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,17 @@
-/*function user(){*/
-	var ingresa_tarea= document.getElementById('ingresa_tarea'),
-	boton= document.getElementById('boton_agregar');
-
-   /* //editar elemento de la lista
+/* //editar elemento de la lista
     for (var i=0; i<=tarea-list.children.length;i++){
       tarea-list.children[i]
     }*/
 // agrega la nueva tarea
 function anadirTarea(){
+var ingresa_tarea= document.getElementById('ingresa_tarea'),
+    boton= document.getElementById('boton_agregar');
+//validar el ingreso de datos    
+var tarea=ingresa_tarea.value;
+  if(tarea===""){
+    ingresa_tarea.setAttribute("placeholder","Añade una Tarea Valida");
+    return false;
+  }
   var input = document.getElementById("ingresa_tarea");
 
   var contTarea = document.getElementsByClassName("tarea-list");
@@ -15,5 +19,4 @@ function anadirTarea(){
   contTarea[0].innerHTML += "<li>"+ input.value +"</li><br>";
 
   input.value = "";
-  
 };
